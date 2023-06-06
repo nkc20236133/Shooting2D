38e194114fc7dc56be4187d2d3732a96a2ebd0fd
nkc20236133 <nkc23.hirukawa.yukiya@gmail.com>
2023-06-06 15:11:19 +0900

    6/62回目更新

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector3 dir = Vector3.zero; //ˆÚ“®•ûŒü‚ğ•Û‘¶‚·‚é•Ï”
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float speed = 10;
+
+        //ˆÚ“®•ûŒü‚ğƒZƒbƒg
+        dir.x = Input.GetAxisRaw("Horizontal");
+        dir.y = Input.GetAxisRaw("Vertical");
+
+        transform.position += dir.normalized* speed * Time.deltaTime;
+
+        //‰æ–Ê“àˆÚ“®§ŒÀ
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, -9f, 9f);
+        pos.y = Mathf.Clamp(pos.y, -5f, 5f);
+        transform.position = pos;
+
+    }
+}
